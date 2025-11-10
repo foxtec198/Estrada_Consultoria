0c8030a5b026266fa44d7a4f5abd4f49586a44fe
Guilherme Breve <64221923+foxtec198@users.noreply.github.com>
2025-11-10 00:17:27 -0300

    Updt: Sendo form by API

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var spinner = '<span id="spin_ldg" class="spinner-border spinner-border-sm text-light" role="status"></span>'
+const mail = "foxtec198@gmail.com"
+
 // Simples filtro de serviços com select
 document.getElementById('serviceSelect').addEventListener('change', function (e) {
     const val = e.target.value.toLowerCase();
@@ -8,13 +11,6 @@ document.getElementById('serviceSelect').addEventListener('change', function (e)
     });
 });
 
-// Envio de form fictício
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Obrigado! Sua mensagem foi enviada (simulação).');
-    this.reset();
-});
-
 window.onload = function () {
     var recaptcha = document.forms["contactForm"]["g-recaptcha-response"];
     recaptcha.required = true;
@@ -23,3 +19,48 @@ window.onload = function () {
     alert("Por favor, complete o reCAPTCHA para prosseguir.");
     }
 }
+
+document.getElementById('contactForm').addEventListener('submit', async function (e){
+    e.preventDefault()
+
+    const nome = this.nome.value
+    const email = this.email.value
+    const tel = this.telefone.value
+    const msg = this.msg.value 
+    if(msg === ''){"Entre em contato comigo"}
+    const btn = this.btnSub
+
+    if(nome && tel && email ){
+        const html = `
+            <h1>Você tem um novo lead!</h1>
+            <p>Entre em contato para mais detalhes!</p>
+            <hr>
+            <br>
+            <p><strong>Nome: </strong>${nome}</p>
+            <p><strong>Telefone: </strong>${tel}</p>
+            <p><strong>Email: </strong>${email}</p>
+            <p><strong>Mensagem: </strong>${msg}</p>
+            <a style="background: #5E8B60; color: #fff; padding: 8px; border-radius: 8px; width: 100%;" href="https://wa.me/${tel}">Enviar mensagem no WhatsApp!</a>
+            <br>
+            <br>
+            <hr>
+            <a style="color: #5E8B60; text-decoration: none;" href="https://tecnobreve.onrender.com">© Desenvolvido por Tecnobreve, 2025</a>
+        `
+    
+        btn.innerHTML = spinner
+        const req = await fetch(
+            `https://api.hubbix.com.br/send_mail/${mail}`, 
+            {
+                method: 'POST', 
+                headers: {"Content-Type": "application/json"}, 
+                body: JSON.stringify({
+                    "title": `Novo pedido de Visita - ${nome}`,
+                    "html": html
+                })
+            }
+        )
+        const res = await req.json()
+        if(req.ok){btn.innerHTML = res}
+        else{alert("Erro na requisição.")}
+    }else{alert('Dados não informados corretamente!')}
+})
